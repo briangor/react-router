1dbab827f1a2e8b269c67ddea344808364bd0f92
briangor <briangor13@gmail.com>
2024-04-23 11:55:50 +0300

    update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,37 +1,70 @@
-import { Outlet, Link, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { useEffect } from "react";
+import { Outlet, NavLink, Link, useLoaderData, Form, redirect, useNavigation, useSubmit } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
-export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
+    const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching = 
+        navigation.location && 
+        new URLSearchParams(navigation.location.search).has("q");
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
+
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
-                        <input type="search" id="q" aria-label="Search contacts" placeholder="Search" name="q" />
-                        <div id="search-spinner" aria-hidden hidden={true} />
+                    <Form id="search-form" role="search">
+                        <input 
+                        type="search" 
+                        id="q"
+                        className={searching ? "loading" : ""} 
+                        aria-label="Search contacts" 
+                        placeholder="Search" 
+                        name="q" 
+                        defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                        />
+                        <div id="search-spinner" aria-hidden hidden={!searching} />
                         <div className="sr-only" aria-live="polite"></div>
-                    </form>
-                    <form method="post">
+                    </Form>
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {/* <ul>
                         <li><Link to={`contacts/1`}>Your Name</Link></li>
                         <li><Link to={`contacts/2`}>Your Friend</Link></li>
                     </ul> */}
-                    { contacts.length ? (
+                    {contacts.length ? (
                         <ul>
-                            { contacts.map((contact) => (
+                            {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+                                    {/* <Link to={`contacts/${contact.id}`}>
                                         { contact.first || contact.last? (
                                             <>
                                             { contact.first } { contact.last }
@@ -40,7 +73,22 @@ export default function Root() {
                                             <i>No Name</i>
                                         )}{ " "}
                                         { contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </Link> */}
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive ? "active" : isPending ? "pending" : ""
+                                        }
+                                    >
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
@@ -51,7 +99,12 @@ export default function Root() {
                     )}
                 </nav>
             </div>
-            <div id="detail">
+            <div
+                id="detail"
+                className={
+                    navigation.state === "loading" ? "loading" : ""
+                }
+            >
                 <Outlet />
             </div>
         </>
